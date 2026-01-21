ee521822904f5ab473a388082622d5252ff4dca6
Aadesh Ramai <aadeshramai17@gmail.com>
2026-01-21 23:18:06 +0100

    double plot custom text

--- inference/text/text_plot_hist_cs.jl
+++ inference/text/text_plot_hist_cs.jl
@@ -3,7 +3,6 @@ include("../../common/utils.jl")
 
 cs = custom_text_inference_mh_gibbs(observations; niter=20_000, burnin=5_000, thin=10)
 
-# p_cs_tau = tau_pmf_plot(cs.taus, N; title="Custom: τ PMF")
-# display(p_cs_tau)
-
+p_cs_tau = tau_pmf_plot(cs.taus, N; title="Custom: τ PMF")
 tau_plot = plot(cs.taus, title="Running mean of tau", xlabel="Iteration / Sample Index", ylabel="E[tau]", label="Custom")
+combined = plot(p_cs_tau, tau_plot,layout=(1,2), size=(1100, 550), margin=5Plots.mm)
